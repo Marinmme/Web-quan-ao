7ab30e05c31be95143a341c07b0546f14541b2df
Marinmme <nguyenconghainam2@gmail.com>
2021-12-19 22:47:47 +0800

    Chức năng đăng ký

--- register.php
+++ register.php
@@ -7,348 +7,143 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html>
 <head>
-<title>Shopin A Ecommerce Category Flat Bootstrap Responsive Website Template | Register :: w3layouts</title>
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-<!-- Custom Theme files -->
-<!--theme-style-->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
-<!--//theme-style-->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Shopin Responsive web template, Bootstrap Web Templates, Flat Web Templates, AndroId Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--theme-style-->
-<link href="css/style4.css" rel="stylesheet" type="text/css" media="all" />	
-<!--//theme-style-->
-<script src="js/jquery.min.js"></script>
-<!--- start-rate---->
-<script src="js/jstarbox.js"></script>
-	<link rel="stylesheet" href="css/jstarbox.css" type="text/css" media="screen" charset="utf-8" />
-		<script type="text/javascript">
-			jQuery(function() {
-			jQuery('.starbox').each(function() {
-				var starbox = jQuery(this);
-					starbox.starbox({
-					average: starbox.attr('data-start-value'),
-					changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
-					ghosting: starbox.hasClass('ghosting'),
-					autoUpdateAverage: starbox.hasClass('autoupdate'),
-					buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
-					stars: starbox.attr('data-star-count') || 5
-					}).bind('starbox-value-changed', function(event, value) {
-					if(starbox.hasClass('random')) {
-					var val = Math.random();
-					starbox.next().text(' '+val);
-					return val;
-					} 
-				})
-			});
-		});
-		</script>
+<?php include('layouts/header.php') ?>
+
 <!---//End-rate---->
+
 </head>
 <body>
 <!--header-->
-<div class="header">
-<div class="container">
-		<div class="head">
-			<div class=" logo">
-				<a href="index.html"><img src="images/logo.png" alt=""></a>	
-			</div>
-		</div>
-	</div>
-	<div class="header-top">
-		<div class="container">
-		<div class="col-sm-5 col-md-offset-2  header-login">
-					<ul >
-						<li><a href="login.html">Login</a></li>
-						<li><a href="register.html">Register</a></li>
-						<li><a href="checkout.html">Checkout</a></li>
-					</ul>
-				</div>
-				
-			<div class="col-sm-5 header-social">		
-					<ul >
-						<li><a href="#"><i></i></a></li>
-						<li><a href="#"><i class="ic1"></i></a></li>
-						<li><a href="#"><i class="ic2"></i></a></li>
-						<li><a href="#"><i class="ic3"></i></a></li>
-						<li><a href="#"><i class="ic4"></i></a></li>
-					</ul>
-					
-			</div>
-				<div class="clearfix"> </div>
-		</div>
-		</div>
-		
-		<div class="container">
-		
-			<div class="head-top">
-			
-		 <div class="col-sm-8 col-md-offset-2 h_menu4">
-				<nav class="navbar nav_bottom" role="navigation">
- 
- <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header nav_2">
-      <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-     
-   </div> 
-   <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-        <ul class="nav navbar-nav nav_1">
-            <li><a class="color" href="index.html">Home</a></li>
-            
-    	<li class="dropdown mega-dropdown active">
-			    <a class="color1" href="#" class="dropdown-toggle" data-toggle="dropdown">Women<span class="caret"></span></a>				
-				<div class="dropdown-menu">
-                    <div class="menu-top">
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu1</h4>
-									<ul>
-										<li><a href="product.html">Accessories</a></li>
-										<li><a href="product.html">Bags</a></li>
-										<li><a href="product.html">Caps & Hats</a></li>
-										<li><a href="product.html">Hoodies & Sweatshirts</a></li>
-										
-									</ul>	
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu2</h4>
-								<ul>
-										<li><a href="product.html">Jackets & Coats</a></li>
-										<li><a href="product.html">Jeans</a></li>
-										<li><a href="product.html">Jewellery</a></li>
-										<li><a href="product.html">Jumpers & Cardigans</a></li>
-										<li><a href="product.html">Leather Jackets</a></li>
-										<li><a href="product.html">Long Sleeve T-Shirts</a></li>
-									</ul>	
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu3</h4>
-									<ul>
-										<li><a href="product.html">Shirts</a></li>
-										<li><a href="product.html">Shoes, Boots & Trainers</a></li>
-										<li><a href="product.html">Sunglasses</a></li>
-										<li><a href="product.html">Sweatpants</a></li>
-										<li><a href="product.html">Swimwear</a></li>
-										<li><a href="product.html">Trousers & Chinos</a></li>
-										
-									</ul>	
-								
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu4</h4>
-								<ul>
-									<li><a href="product.html">T-Shirts</a></li>
-									<li><a href="product.html">Underwear & Socks</a></li>
-									<li><a href="product.html">Vests</a></li>
-									<li><a href="product.html">Jackets & Coats</a></li>
-									<li><a href="product.html">Jeans</a></li>
-									<li><a href="product.html">Jewellery</a></li>
-								</ul>	
-							</div>							
-						</div>
-						<div class="col1 col5">
-						<img src="images/me.png" class="img-responsive" alt="">
-						</div>
-						<div class="clearfix"></div>
-					</div>                  
-				</div>				
-			</li>
-			<li class="dropdown mega-dropdown active">
-			    <a class="color2" href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>				
-				<div class="dropdown-menu mega-dropdown-menu">
-                    <div class="menu-top">
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu1</h4>
-									<ul>
-										<li><a href="product.html">Accessories</a></li>
-										<li><a href="product.html">Bags</a></li>
-										<li><a href="product.html">Caps & Hats</a></li>
-										<li><a href="product.html">Hoodies & Sweatshirts</a></li>
-										
-									</ul>	
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu2</h4>
-								<ul>
-										<li><a href="product.html">Jackets & Coats</a></li>
-										<li><a href="product.html">Jeans</a></li>
-										<li><a href="product.html">Jewellery</a></li>
-										<li><a href="product.html">Jumpers & Cardigans</a></li>
-										<li><a href="product.html">Leather Jackets</a></li>
-										<li><a href="product.html">Long Sleeve T-Shirts</a></li>
-									</ul>	
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu3</h4>
-								
-<ul>
-										<li><a href="product.html">Shirts</a></li>
-										<li><a href="product.html">Shoes, Boots & Trainers</a></li>
-										<li><a href="product.html">Sunglasses</a></li>
-										<li><a href="product.html">Sweatpants</a></li>
-										<li><a href="product.html">Swimwear</a></li>
-										<li><a href="product.html">Trousers & Chinos</a></li>
-										
-									</ul>	
-								
-							</div>							
-						</div>
-						<div class="col1">
-							<div class="h_nav">
-								<h4>Submenu4</h4>
-								<ul>
-									<li><a href="product.html">T-Shirts</a></li>
-									<li><a href="product.html">Underwear & Socks</a></li>
-									<li><a href="product.html">Vests</a></li>
-									<li><a href="product.html">Jackets & Coats</a></li>
-									<li><a href="product.html">Jeans</a></li>
-									<li><a href="product.html">Jewellery</a></li>
-								</ul>	
-							</div>							
-						</div>
-						<div class="col1 col5">
-						<img src="images/me1.png" class="img-responsive" alt="">
-						</div>
-						<div class="clearfix"></div>
-					</div>                  
-				</div>				
-			</li>
-			<li><a class="color3" href="product.html">Sale</a></li>
-			<li><a class="color4" href="404.html">About</a></li>
-            <li><a class="color5" href="typo.html">Short Codes</a></li>
-            <li ><a class="color6" href="contact.html">Contact</a></li>
-        </ul>
-     </div><!-- /.navbar-collapse -->
+<?php include('layouts/headertop.php') ?>
+<?php include("admincp/config/connect.php")?>
+<!--register-->
 
-</nav>
-			</div>
-			<div class="col-sm-2 search-right">
-				<ul class="heart">
-				<li>
-				<a href="wishlist.html" >
-				<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-				</a></li>
-				<li><a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i></a></li>
-					</ul>
-					<div class="cart box_1">
-						<a href="checkout.html">
-						<h3> <div class="total">
-							<span class="simpleCart_total"></span></div>
-							<img src="images/cart.png" alt=""/></h3>
-						</a>
-						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
-
-					</div>
-					<div class="clearfix"> </div>
-					
-						<!----->
-
-						<!---pop-up-box---->					  
-			<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
-			<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-			<!---//pop-up-box---->
-			<div id="small-dialog" class="mfp-hide">
-				<div class="login-search">
-					<div class="login">
-						<input type="submit" value="">
-						<input type="text" value="Search.." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search..';}">		
-					</div>
-					<p>Shopin</p>
-				</div>				
-			</div>
-		 <script>
-			$(document).ready(function() {
-			$('.popup-with-zoom-anim').magnificPopup({
-			type: 'inline',
-			fixedContentPos: false,
-			fixedBgPos: true,
-			overflowY: 'auto',
-			closeBtnInside: true,
-			preloader: false,
-			midClick: true,
-			removalDelay: 300,
-			mainClass: 'my-mfp-zoom-in'
-			});
-																						
-			});
-		</script>		
-						<!----->
-			</div>
-			<div class="clearfix"></div>
-		</div>	
-	</div>	
-</div>
 <!--banner-->
 <div class="banner-top">
 	<div class="container">
 		<h1>Register</h1>
 		<em></em>
-		<h2><a href="index.html">Home</a><label>/</label>Register</h2>
+		<h2><a href="index.php">Home</a><label>/</label>Register</h2>
 	</div>
 </div>
+<!-- <?php
+// define variables and set to empty values
+$nameErr = $emailErr = $phoneErr = $passErr = $addressErr = "";
+$name = $email = $phone = $comment = $pass = $address = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["Email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["Email"]);
+  }
+  if (empty($_POST["diachi"])) {
+    $addressErr = "Address is required";
+  } else {
+    $address = test_input($_POST["diachi"]);
+  }
+    
+
+  if (empty($_POST["PhoneNumber"])) {
+    $phoneErr = "Phone is required";
+  } else {
+    $phone = test_input($_POST["PhoneNumber"]);
+  }
+  if (empty($_POST["Password"])) {
+    $passErr = "Pass is required";
+  } else {
+    $pass = test_input($_POST["Password"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>-->
+<!-- header('Location: index.php?action=checkout');-->
+<?php
+ob_start();
+    if(isset($_POST['customer'])){
+        $tenkhachhang = $_POST['name'];
+		$email = $_POST['Email'];
+        $diachi = $_POST['diachi'];
+		$matkhau = md5($_POST['Password']);
+		$dienthoai = $_POST['PhoneNumber'];
+        $sql_dangky=mysqli_query($mysqli,"insert into tbl_customer(tenkhachhang,email,diachi,dienthoai,matkhau) value('".$tenkhachhang."','".$email."','".$diachi."','".$dienthoai."','".$matkhau."')");
+        if($sql_dangky){
+			$_SESSION['customer'] = $tenkhachhang;
+			$_SESSION['id_khachhang'] = mysqli_insert_id($mysqli);
+		
+			echo "<script> window.location.href='checkout.php'</script>;";
+			
+		}
+    }
+?>
+
 <!--login-->
 <div class="container">
 		<div class="login">
-			<form>
+			<form action="" method="POST">
 			<div class="col-md-6 login-do">
+			<span class="error">* <?php echo $nameErr;?></span>
 			<div class="login-mail">
-					<input type="text" placeholder="Name" required="">
-					<i  class="glyphicon glyphicon-user"></i>
+				
+					<input type="text" placeholder="name" name="name">
+					
+					
 				</div>
+				
+			
+				
+				<span class="error">* <?php echo $emailErr;?></span>
 				<div class="login-mail">
-					<input type="text" placeholder="Phone Number" required="">
-					<i  class="glyphicon glyphicon-phone"></i>
-				</div>
-				<div class="login-mail">
-					<input type="text" placeholder="Email" required="">
+					<input type="text" placeholder="Email"  name="Email" >
 					<i  class="glyphicon glyphicon-envelope"></i>
 				</div>
+				<span class="error">* <?php echo $emailErr;?></span>
 				<div class="login-mail">
-					<input type="password" placeholder="Password" required="">
+					<input type="text" placeholder="Diachi"  name="diachi" >
+					<i  class="glyphicon glyphicon-envelope"></i>
+				</div>
+				<span class="error">* <?php echo $passErr;?></span>
+				<div class="login-mail">
+					<input type="password" placeholder="Password" name="Password" >
 					<i class="glyphicon glyphicon-lock"></i>
 				</div>
+				<span class="error">* <?php echo $phoneErr;?></span>
+				<div class="login-mail">
+					
+					<input type="text" placeholder="PhoneNumber" name="PhoneNumber">
+					<i  class="glyphicon glyphicon-phone"></i>
+				</div>
 				   <a class="news-letter " href="#">
-						 <label class="checkbox1"><input type="checkbox" name="checkbox" ><i> </i>Forget Password</label>
+						 <label class="checkbox1"><input type="text" name="checkbox" ><i> </i>Forget Password</label>
 					   </a>
 				<label class="hvr-skew-backward">
-					<input type="submit" value="Submit">
+					<input type="submit" value="Submit" name="customer">
+				</label>
+				<label class="hvr-skew-backward">
+				<td><a href="#">Đăng nhập nếu có tài khoản</a></td>
 				</label>
 			
 			</div>
-			<div class="col-md-6 login-right">
-				 <h3>Completely Free Account</h3>
-				 
-				 <p>Pellentesque neque leo, dictum sit amet accumsan non, dignissim ac mauris. Mauris rhoncus, lectus tincidunt tempus aliquam, odio 
-				 libero tincidunt metus, sed euismod elit enim ut mi. Nulla porttitor et dolor sed condimentum. Praesent porttitor lorem dui, in pulvinar enim rhoncus vitae. Curabitur tincidunt, turpis ac lobortis hendrerit, ex elit vestibulum est, at faucibus erat ligula non neque.</p>
-				<a href="login.html" class="hvr-skew-backward">Login</a>
-
-			</div>
+			
 			
 			<div class="clearfix"> </div>
 			</form>
 		</div>
 
 </div>
+
 
 <!--//login-->
 
@@ -374,70 +169,60 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	
 	<!--//content-->
 		<!--//footer-->
-	<div class="footer">
-	<div class="footer-middle">
-				<div class="container">
-					<div class="col-md-3 footer-middle-in">
-						<a href="index.html"><img src="images/log.png" alt=""></a>
-						<p>Suspendisse sed accumsan risus. Curabitur rhoncus, elit vel tincidunt elementum, nunc urna tristique nisi, in interdum libero magna tristique ante. adipiscing varius. Vestibulum dolor lorem.</p>
-					</div>
-					
-					<div class="col-md-3 footer-middle-in">
-						<h6>Information</h6>
-						<ul class=" in">
-							<li><a href="404.html">About</a></li>
-							<li><a href="contact.html">Contact Us</a></li>
-							<li><a href="#">Returns</a></li>
-							<li><a href="contact.html">Site Map</a></li>
-						</ul>
-						<ul class="in in1">
-							<li><a href="#">Order History</a></li>
-							<li><a href="wishlist.html">Wish List</a></li>
-							<li><a href="login.html">Login</a></li>
-						</ul>
-						<div class="clearfix"></div>
-					</div>
-					<div class="col-md-3 footer-middle-in">
-						<h6>Tags</h6>
-						<ul class="tag-in">
-							<li><a href="#">Lorem</a></li>
-							<li><a href="#">Sed</a></li>
-							<li><a href="#">Ipsum</a></li>
-							<li><a href="#">Contrary</a></li>
-							<li><a href="#">Chunk</a></li>
-							<li><a href="#">Amet</a></li>
-							<li><a href="#">Omnis</a></li>
-						</ul>
-					</div>
-					<div class="col-md-3 footer-middle-in">
-						<h6>Newsletter</h6>
-						<span>Sign up for News Letter</span>
-							<form>
-								<input type="text" value="Enter your E-mail" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Enter your E-mail';}">
-								<input type="submit" value="Subscribe">	
-							</form>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-			<div class="footer-bottom">
-				<div class="container">
-					<ul class="footer-bottom-top">
-						<li><a href="#"><img src="images/f1.png" class="img-responsive" alt=""></a></li>
-						<li><a href="#"><img src="images/f2.png" class="img-responsive" alt=""></a></li>
-						<li><a href="#"><img src="images/f3.png" class="img-responsive" alt=""></a></li>
-					</ul>
-					<p class="footer-class">&copy; 2016 Shopin. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
+	<?php include('layouts/footer.php') ?>
 		<!--//footer-->
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 
 	<script src="js/simpleCart.min.js"> </script>
 <!-- slide -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
- 
+
+<!--<script type="text/javascript">
+
+ $(document).ready(function() {
+	
+	//Khi bàn phím được nhấn và thả ra thì sẽ chạy phương thức này
+	$("#formDemo").validate({
+				rules: {
+					Name: "required",
+				
+					PhoneNumber: {
+						required: true,
+						minlength: 5
+					},
+				
+					Email: {
+						required: true,
+						email: true
+					},
+					Password: {
+						required: true,
+						minlength: 5
+					},
+		
+				},
+				messages: {
+					Name: "Vui lòng nhập họ",
+				
+					PhoneNumber: {
+						required: "Vui lòng nhập số điện thoại",
+						minlength: "Số máy quý khách vừa nhập là số không có thực"
+					},
+					Password: {
+						required: 'Vui lòng nhập mật khẩu',
+						minlength: 'Vui lòng nhập ít nhất 5 kí tự'
+					},
+					Email: {
+						required: "Please provide a password",
+						minlength: "Your password must be at least 5 characters long",
+						equalTo: "Please enter the same password as above"
+					},
+					email: "Vui lòng nhập Email",
+					agree: "Vui lòng đồng ý các điều khoản"
+				}
+			});
+});-->
+</script>
 </body>
 </html>
