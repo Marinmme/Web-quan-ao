b7e1136ce3eb7ba62f05f07e22f8e0e01558a1a0
Marinmme <nguyenconghainam2@gmail.com>
2021-12-20 01:04:01 +0800

    Trang đăng nhập admin

--- admincp/login.php
+++ admincp/login.php
@@ -1,19 +1,31 @@
+<?php
+session_start();
+include('config/connect.php');
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+if(isset($_POST['dangnhap'])){
+    $taikhoan=$_POST['username'];
+    $matkhau=md5($_POST['password']);
+    $sql="select *from tbl_admin where username='".$taikhoan."' and password='".$matkhau."' limit 1 ";
+    $row=mysqli_query($mysqli,$sql);
+    $count = mysqli_num_rows($row);
+    if($count>0){
+        $_SESSION['dangnhap']=$taikhoan;
+        header("location:index.php?action=admin&query=admin");
+    }else{
+        header('Location: login.php');
+            $_SESSION['dangnhap1'] = 'Username and Password error !';
+    }
+ 
+}
+?>
+ 
+
 <!DOCTYPE html>
 <html>
 
 <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Start Bootstrap - SB Admin Version 2.0 Demo</title>
-
-    <!-- Core CSS - Include with every page -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <!-- SB Admin CSS - Include with every page -->
-    <link href="css/sb-admin.css" rel="stylesheet">
+   <?php include('layouts/header.php') ?>
 
 </head>
 
@@ -27,29 +39,44 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
-                            </fieldset>
-                        </form>
+                    <form action="" autocomplete="off" method="POST" onsubmit="">
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="text" class="form-control" placeholder="Email" name="username" require="true" >
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password" name="password" >
+                        </div>
+                 
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+                            <label class="pull-right">
+                                <a href="#">Forgotten Password?</a>
+                            </label>
+                            
+                        </div>
+                        
+                        <button type="submit" name="dangnhap">Sign in</button> <?php
+                                                //nếu có session tên dangnhap thì ta thực hiện lệnh dưới
+                                                if(isset($_SESSION['dangnhap1']) && $_SESSION['dangnhap1'] != NULL)
+                                                {
+                                                    echo '<h4 style="color: red"> '.$_SESSION['dangnhap1'];' </h4>';
+                                                    
+                                                }
+                                            ?> 
+                        <div class="social-login-content">
+                        </div>
+                       
+                    </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
+   
     <!-- Core Scripts - Include with every page -->
     <script src="js/jquery-1.10.2.js"></script>
     <script src="js/bootstrap.min.js"></script>
